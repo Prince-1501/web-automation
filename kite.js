008213b09ef3218382305d4328a41dd2831303d3
iamprince <prince.kumar@skill-mine.com>
2021-10-29 15:01:58 +0530

    intraday click done

--- kite.js
+++ kite.js
@@ -1,7 +1,6 @@
 const puppeteer = require('puppeteer');
 require('dotenv').config();
 
-
 (async () => {
   const browser = await puppeteer.launch({ headless: false , args: ["--disable-notifications"]});
   const page = await browser.newPage();
@@ -11,8 +10,8 @@ require('dotenv').config();
 
   // await page.goto('https://kite.zerodha.com/holdings');
   await page.goto('https://kite.zerodha.com/');
-  await page.setViewport({ width: 1920, height: 1080 })
-
+  //await page.setViewport({ width: 1200, height: 800 })
+  await page.setViewport({ width: 1366, height: 625 })
   // login user
   await page.type('#userid', userId);
   await page.type('#password', password);
@@ -22,26 +21,44 @@ require('dotenv').config();
 
   // Enter Authentication OTP
   await page.waitForSelector('#totp')
-  await page.type('#totp', '864749')
+  await page.type('#totp', '524495')
 
   await page.waitForSelector('.row > .login-form > .twofa-form > .actions > .button-orange')
   await page.click('.row > .login-form > .twofa-form > .actions > .button-orange')
 
+
+  // search the Ticker Name
   await page.waitForSelector('.marketwatch-sidebar > .omnisearch > .search > .su-input-group > input')
-  await page.type('.marketwatch-sidebar > .omnisearch > .search > .su-input-group > input', 'SBIN');
+  await page.type('.marketwatch-sidebar > .omnisearch > .search > .su-input-group > input', 'fsl');
 
   // await page.click('.marketwatch-sidebar > .omnisearch > .search > .su-input-group > input', 'SBIN')
 
-  const imageItems = await page.hover('.marketwatch-sidebar > .omnisearch > .search > .su-input-group > input');
-  console.log(imageItems);
+  await page.waitForSelector('.omnisearch-results > div > .search-result-item')
+  await page.click('.omnisearch-results > div > .search-result-item')
 
-  await page.mouse.click(40, 100);
+  /* Future Uses
+  await page.waitForSelector('.omnisearch-results > div > .search-result-item > .action-buttons > .button-blue')
+  await page.click('.omnisearch-results > div > .search-result-item > .action-buttons > .button-blue')
+  
+  */
 
-  await page.waitForSelector('.omnisearch-results > div > .search-result-item selected > .action-buttons > .button-blue')
-  await page.click('.omnisearch-results > div > .search-result-item selected > .action-buttons > .button-blue')
 
+  // dialog box opens Now choose the options Below
+
+  // 1. Intraday
+  await page.waitForSelector('.body > .product > .su-radio-group > .type:nth-child(1) > .su-radio-label')
+  await page.click('.body > .product > .su-radio-group > .type:nth-child(1) > .su-radio-label')
+
+
+  // 2. Quantity
+  await page.waitForSelector('.fields > .row > .quantity > .no > input')
+  await page.type('.fields > .row > .quantity > .no > input', '639')
+
+  // 3. Price
+  await page.waitForSelector('.fields > .row > .price > .no > input')
+  await page.click({ clickCount: 3 })
+  await page.type('.fields > .row > .price > .no > input', '400')
 
   //await page.screenshot({ path: 'sc.png', fullPage: true });
-
   //await browser.close();
 })();
